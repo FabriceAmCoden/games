1dd7767fa168b42f8b07e399326de8e0c83265b8
Fabrice <Fabrice>
2023-05-22 12:48:11 +0200

    videos zum verständnis

---
+++ src/at/Faepse/test/Rectangles.java
@@ -0,0 +1,29 @@
+package at.Faepse.test;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Rectangles extends BasicGame {
+
+
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
--- src/org/newdawn/slick/tests/AnimationTest.java
+++ src/org/newdawn/slick/tests/AnimationTest.java
@@ -119,3 +119,4 @@ public class AnimationTest extends BasicGame {
 		}
 	}
 }
+
